03643d5b515b973d9f0e333d623163e2e2473647
flicksolutions <sebastian@flick.solutions>
2026-07-20 17:28:32 +0200

    move all collections infos to the same place

--- src/routes/collections/+page.ts
+++ src/routes/collections/+page.ts
@@ -1,9 +1,9 @@
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = async () => {
-	const files = import.meta.glob('./**/structure.json', { eager: true });
+	const files = import.meta.glob('../../lib/collections/**/structure.json', { eager: true });
 	const collections = Object.keys(files)
-		.map((path) => path.match(/\.\/(?<slug>[^/]+)\/structure\.json$/)?.groups?.slug)
+		.map((path) => path.match(/collections\/(?<slug>[^/]+)\/structure\.json$/)?.groups?.slug)
 		.filter((slug): slug is string => typeof slug === 'string' && slug !== '');
 
 	return { collections };
--- src/routes/collections/[collection]/+page.ts
+++ src/routes/collections/[collection]/+page.ts
@@ -1,15 +1,17 @@
 import { error } from '@sveltejs/kit';
 import { loadCollection } from '$lib/collectionLoad';
-import type { CollectionPageData, ItemStructure } from '../../types';
+import type { ItemStructure } from '../../types';
 import type { PageLoad, EntryGenerator } from './$types';
 
 export const load: PageLoad = async ({ params }) => {
 	const { collection } = params;
 
 	try {
-		const structure: ItemStructure[] = (await import(`../${collection}/structure.json`)).default;
-		const Content = (await import(`../${collection}/content.md`)).default;
-		const items: unknown[] = (await import(`$lib/collections/${collection}.json`)).default;
+		const structure: ItemStructure[] = (
+			await import(`$lib/collections/${collection}/structure.json`)
+		).default;
+		const Content = (await import(`$lib/collections/${collection}/content.md`)).default;
+		const items: unknown[] = (await import(`$lib/collections/${collection}/data.json`)).default;
 
 		return loadCollection(collection, structure, Content, items);
 	} catch (err) {
@@ -19,9 +21,9 @@ export const load: PageLoad = async ({ params }) => {
 };
 
 export const entries: EntryGenerator = () => {
-	const files = import.meta.glob('../**/structure.json', { eager: true });
+	const files = import.meta.glob('../../lib/collections/**/structure.json', { eager: true });
 	const slugs = Object.keys(files)
-		.map((path) => path.match(/\.\.\/(?<slug>[^/]+)\/structure\.json$/)?.groups?.slug)
+		.map((path) => path.match(/collections\/(?<slug>[^/]+)\/structure\.json$/)?.groups?.slug)
 		.filter((slug): slug is string => typeof slug === 'string' && slug !== '');
 
 	return slugs.map((slug) => ({ collection: slug }));
